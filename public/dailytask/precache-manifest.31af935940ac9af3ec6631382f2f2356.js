188beb4aa1b553b998cb814a55a61b9c07058816
Adam Hollister <ah@pragmatic-web.co.uk>
2018-07-26 00:06:52 +0100

    trying to fix service worker path

--- public/dailytask/precache-manifest.31af935940ac9af3ec6631382f2f2356.js
+++ public/dailytask/precache-manifest.31af935940ac9af3ec6631382f2f2356.js
@@ -1,26 +1,26 @@
 self.__precacheManifest = [
   {
     "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
-    "url": "/robots.txt"
+    "url": "./robots.txt"
   },
   {
     "revision": "94591c5d51bf20f62802",
-    "url": "/js/chunk-vendors.94591c5d.js"
+    "url": "./js/chunk-vendors.94591c5d.js"
   },
   {
     "revision": "b2d57b4b7c7d87017801",
-    "url": "/js/app.b2d57b4b.js"
+    "url": "./js/app.b2d57b4b.js"
   },
   {
     "revision": "a1a3cb12db373e9ee4515212d076e35e",
-    "url": "/index.html"
+    "url": "./index.html"
   },
   {
     "revision": "94591c5d51bf20f62802",
-    "url": "/css/chunk-vendors.d4819308.css"
+    "url": "./css/chunk-vendors.d4819308.css"
   },
   {
     "revision": "b2d57b4b7c7d87017801",
-    "url": "/css/app.178c22ce.css"
+    "url": "./css/app.178c22ce.css"
   }
 ];
